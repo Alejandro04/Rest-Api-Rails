c15a97a33c55d50064698f1be2c93f118f305ae3
Alejandro Roa <alejandro@htdevs.com>
2019-01-02 19:08:05 -0400

    refactor subcategories controlller

--- app/controllers/api/v1/subcategories_controller.rb
+++ app/controllers/api/v1/subcategories_controller.rb
@@ -1,48 +1,50 @@
 module Api
     module V1
         class SubcategoriesController < ApplicationController
-            def index
-                subcategories = Subcategory.order('created_at DESC');
-                render json: {status: 'SUCCESS', message: 'Loaded subcategories', data:subcategories},status: :ok
-            end
+          #before_action :authenticate_user!
+          before_action :set_subcategory, only: [:show, :update, :destroy]
 
-            def show
-                subcategory = Subcategory.find(params[:id])
-                render json: {status: 'SUCCESS', message: 'Loaded subcategory', data:subcategory},status: :ok
+          def index
+            @subcategories = Subcategory.all
+        
+            render json: @subcategories
+          end
+        
+          def show
+            render json: @subcategory
+          end
+        
+          def create
+            @subcategory = Subcategory.new(subcategory_params)
+        
+            if @subcategory.save
+              render json: @subcategory, status: :created
+            else
+              render json: @subcategory.errors, status: :unprocessable_entity
             end
-
-            def create
-                subcategory = Subcategory.new(subcategory_params)
-                
-                if subcategory.save
-                    render json: {status: 'SUCCESS', message: 'Loaded subcategory', data:subcategory},status: :ok
-                else
-                    render json: {status: 'ERROR', message: 'SubCategory not saved', 
-                    data:subcategory.errors},status: :unprocessable_entity
-                end   
+          end
+        
+          def update
+            if @subcategory.update(subcategory_params)
+              render json: @subcategory
+            else
+              render json: @subcategory.errors, status: :unprocessable_entity
             end
-
-            def destroy 
-                subcategory = Subcategory.find(params[:id])
-                subcategory.destroy
-                render json: {status: 'SUCCESS', message: 'Deleted subcategory', data:subcategory},status: :ok
-            end
-
-            def update
-                subcategory = Subategory.find(params[:id])
-                if subcategory.update_attributes(subcategory_params)
-                    render json: {status: 'SUCCESS', message: 'Updated subcategory', data:subcategory},status: :ok
-                else
-                    render json: {status: 'ERROR', message: 'SubCategory not updated', 
-                    data:subcategory.errors},status: :unprocessable_entity
-                end   
-            end
-
+          end
+        
+          def destroy
+            @subcategory.destroy
+          end        
+      
             private
+            def set_subcategory
+              @subcategory = Subcategory.find(params[:id])
+            end
 
             def subcategory_params
-                params.permit(:name, :category_id)
+                params.permit(:name)
             end
+
         end
     end
 end
