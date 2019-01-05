29109f3c625426d8cc42f69f373ff5252004cad6
Alejandro Roa <alejandro@htdevs.com>
2019-01-05 17:07:49 -0400

    add cors, add subcategories params

--- app/controllers/api/v1/subcategories_controller.rb
+++ app/controllers/api/v1/subcategories_controller.rb
@@ -42,7 +42,7 @@ module Api
             end
 
             def subcategory_params
-                params.permit(:name)
+                params.permit(:name, :category_id)
             end
 
         end
